920b87b02ad723832ee835a5d18bf04c771703d7
HRS-Computer <3474006766@qq.com>
2022-02-20 10:33:56 +0800

    update

---
+++ src/main/java/io/github/kloping/iwanna/buy/api/WareHouse.java
@@ -0,0 +1,62 @@
+package io.github.kloping.iwanna.buy.api;
+
+import java.util.List;
+
+/**
+ * @author github.kloping
+ */
+public interface WareHouse extends Savable<WareHouse> {
+    /**
+     * this warehouse id
+     *
+     * @return
+     */
+    Number getId();
+
+    /**
+     * get this warehouse capacity
+     *
+     * @return
+     */
+    int getCapacity();
+
+    /**
+     * get this warehouse surplus capacity
+     *
+     * @return
+     */
+    int getSurplusCapacity();
+
+    /**
+     * add a commodity into this warehouse
+     * and return current Capacity
+     *
+     * @param commodity
+     * @return Capacity
+     */
+    int add(Commodity commodity);
+
+    /**
+     * current all obj
+     *
+     * @return
+     */
+    List<Commodity> getAll();
+
+    /**
+     * find commodity num
+     *
+     * @param id
+     * @return
+     */
+    int findCommodity(Integer id);
+
+    /**
+     * lose obj
+     *
+     * @param commodity
+     * @param num
+     * @return
+     */
+    WareHouse lose(Commodity commodity, int num);
+}
---
+++ src/main/java/io/github/kloping/iwanna/buy/impl/simple/SimpleSys.java
@@ -0,0 +1,182 @@
+package io.github.kloping.iwanna.buy.impl.simple;
+
+import io.github.kloping.iwanna.buy.api.Bank;
+import io.github.kloping.iwanna.buy.api.Commodity;
+import io.github.kloping.iwanna.buy.api.Event;
+import io.github.kloping.iwanna.buy.api.Shop;
+import io.github.kloping.iwanna.buy.impl.Sys;
+import io.github.kloping.date.FrameUtils;
+import io.github.kloping.file.FileUtils;
+import io.github.kloping.serialize.HMLObject;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.*;
+
+/**
+ * @author github.kloping
+ */
+public class SimpleSys extends Sys {
+    private static int ID = 1000;
+
+    public static SimpleSys INSTANCE;
+    public static final Random RANDOM = new Random();
+
+    private static String commodityFileName = "commodities";
+    private static String eventsFileName = "events";
+    private static String playerFileName = "players";
+    private static String bankFileName = "bankConf.hml";
+    private static String shopFileName = "shopConf.hml";
+    private static String warehouseFileName = "warehouses";
+
+    @Override
+    public int getSerialId() {
+        return getId();
+    }
+
+    public static synchronized int getId() {
+        return ++ID;
+    }
+
+    @Override
+    public int updateSerialId(int id) {
+        return updateId(id);
+    }
+
+    public static synchronized int updateId(int id) {
+        return ID = ID < id ? ID : id;
+    }
+
+    private String path;
+
+    public static SimpleSys factory(String path) {
+        SimpleSys sys = new SimpleSys();
+        SimpleSys.INSTANCE = sys;
+        sys.path = path;
+        sys.shop = new SimpleShop(path);
+        sys.bank = new SimpleBank(path);
+        sys.t = 10 * 1000L;
+        FrameUtils.INSTANCE.getFrames().add(sys);
+        sys.loadEvents();
+        sys.loadCommodity();
+        return sys;
+    }
+
+    private SimpleSys() {
+    }
+
+    private void loadCommodity() {
+        for (File file : new File(path, commodityFileName).listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.getName().endsWith(".hml");
+            }
+        })) {
+            try {
+                String hmlStr = FileUtils.getStringFromFile(file.getAbsolutePath());
+                ConfCommodity confCommodity = HMLObject.parseObject(hmlStr).toJavaObject(ConfCommodity.class);
+                commodityMap.put(confCommodity.getId(), confCommodity);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private List<Event> events = new LinkedList<>();
+    private Map<Integer, Commodity> commodityMap = new HashMap<>();
+
+    @Override
+    protected void loadEvents() {
+        for (File file : new File(path, eventsFileName).listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.getName().endsWith(".hml");
+            }
+        })) {
+            try {
+                String hmlStr = FileUtils.getStringFromFile(file.getAbsolutePath());
+                ConfEvent confEvent = HMLObject.parseObject(hmlStr).toJavaObject(ConfEvent.class);
+                events.add(confEvent);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String basePath() {
+        return path;
+    }
+
+    @Override
+    public String eventsPath() {
+        return eventsFileName;
+    }
+
+    @Override
+    public String warehousePath() {
+        return warehouseFileName;
+    }
+
+    @Override
+    public String playersPath() {
+        return playerFileName;
+    }
+
+    @Override
+    public String commodityPath() {
+        return commodityFileName;
+    }
+
+    @Override
+    public String bankPath() {
+        return bankFileName;
+    }
+
+    @Override
+    public String shopPath() {
+        return shopFileName;
+    }
+
+    @Override
+    public Bank getBank() {
+        return bank;
+    }
+
+    @Override
+    public Shop getShop() {
+        return shop;
+    }
+
+    private int indexEvent = 0;
+
+    @Override
+    public Event getEvent() {
+        Event event = null;
+        if (indexEvent < events.size()) {
+            event = events.get(indexEvent);
+            indexEvent++;
+        } else if (events.size() == indexEvent) {
+            Collections.shuffle(events);
+            indexEvent = 0;
+            event = events.get(indexEvent);
+        }
+        return event;
+    }
+
+    @Override
+    public List<Commodity> commodities() {
+        return new ArrayList<>(commodityMap.values());
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleSys{" +
+                "path='" + path + '\'' +
+                ", events=" + events +
+                ", commodityMap=" + commodityMap +
+                ", indexEvent=" + indexEvent +
+                '}';
+    }
+}
+
